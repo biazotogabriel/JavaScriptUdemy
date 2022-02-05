15c72e66893db643e6cde2f6b1dddce8bf1f1872
Gabriel Biazoto <biazotogabriel@gmail.com>
2022-02-05 14:18:03 -0300

    ok

--- 03-React/react-base/src/services/axios.js
+++ 03-React/react-base/src/services/axios.js
@@ -4,6 +4,6 @@ const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJiaWF6b3RvZ2FicmllbEBnbWFpbC5jb20iLCJpYXQiOjE2NDQwMTQ5NzUsImV4cCI6MTY0NDYxOTc3NX0.yzgpblMf098f9C34RRyIC9xC1bGxVR2EQ8RSU4Q5VkA'
 
 export default axios.create({
-  baseURL: 'http://34.151.227.242/',
+  baseURL: 'COLOQUE_SUA_URL_AQUI', // http://34.151.227.242/
   headers: { Authorization: `Bearer ${token}` },
 })
